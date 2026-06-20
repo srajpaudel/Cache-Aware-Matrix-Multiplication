dc10f3a9777723df132788d02dfa7cecb43bb5a3
Sankalpa Paudel <163387708+srajpaudel@users.noreply.github.com>
2026-06-20 13:28:42 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,4 +7,13 @@ using namespace std;
 using namespace std::chrono;
 
 using Matrix = vector<vector<double>>;
-  
+
+Matrix generate(int n) {
+  // Initialize n x n matrix with random values from [0,1]
+  // Used to generate benchmark input data for testing
+  Matrix m(n, vector<double>(n));
+  random_device rd;
+  mt19937 gen(rd()));
+  uniform_real_distribution<> dist(0.0, 1.0);
+
+
